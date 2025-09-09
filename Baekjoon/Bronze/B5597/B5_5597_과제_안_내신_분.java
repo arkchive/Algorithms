7cc9c9061d3e9e285a4b9686d2cfd911708f1493
arkchive <kyungminkim7@konkuk.ac.kr>
2025-09-09 16:29:13 +0900

    solve: 백준 bronze5 [#5597]  과제 안 내신 분

---
+++ Baekjoon/Bronze/B5597/B5_5597_과제_안_내신_분.java
@@ -0,0 +1,28 @@
+package Baekjoon.Bronze.B5597;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class B5_5597_과제_안_내신_분 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufRead = new BufferedReader(new InputStreamReader(System.in));
+        boolean [] submitted = new boolean[31];
+        
+        for (int i = 0; i < 28; i++) {
+            int n = Integer.parseInt(bufRead.readLine());
+            submitted[n] = true;
+        }
+        
+        List<Integer> miss = new ArrayList<>(2);
+        for (int i = 1; i <= 30; i++) {
+            if (!submitted[i]) miss.add(i);
+        }
+        System.out.println(miss.get(0));
+        System.out.println(miss.get(1));
+    }
+    
+}
