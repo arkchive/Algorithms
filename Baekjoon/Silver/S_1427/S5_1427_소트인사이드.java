586e2b42fe35135f007aa177238a5ec97572017d
arkchive <kyungminkim7@konkuk.ac.kr>
2025-09-19 14:27:12 +0900

    mod: 불필요 코드 삭제

--- Baekjoon/Silver/S_1427/S5_1427_소트인사이드.java
+++ Baekjoon/Silver/S_1427/S5_1427_소트인사이드.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class S5_1427_소트인사이드 {
