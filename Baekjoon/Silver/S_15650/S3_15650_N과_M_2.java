d2aac0ac7bd8ee868bcaedbd960d360810273e6b
arkchive <kyungminkim7@konkuk.ac.kr>
2025-09-30 14:05:58 +0900

    solve: 백준 silver3[#15650] N과 M(2)

---
+++ Baekjoon/Silver/S_15650/S3_15650_N과_M_2.java
@@ -0,0 +1,39 @@
+package Baekjoon.Silver.S_15650;
+
+import java.util.Scanner;
+
+public class S3_15650_N과_M_2 {
+
+    static int N, M;
+    static int[] arr;
+    static StringBuilder strBuild = new StringBuilder();
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        N = scan.nextInt();
+        M = scan.nextInt();
+
+        arr = new int[M];
+
+        backtrack(0, 1);
+        System.out.println(strBuild.toString());
+
+    }
+
+    static void backtrack(int depth, int start) {
+        if (depth == M) {
+            for (int i : arr) {
+                strBuild.append(i).append(" ");
+            }
+            strBuild.append("\n");
+            return;
+        }
+        
+        for (int i = start; i <= N; i++) {
+            arr[depth] = i;
+            backtrack(depth + 1, i + 1);
+        }
+    }
+}
