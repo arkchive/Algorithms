c84edf83a8eec8ada43b74965016c45b317c9c71
arkchive <kyungminkim7@konkuk.ac.kr>
2025-10-07 09:38:16 +0900

    solve: 백준 silver3 [#2579]  계단 오르기

---
+++ Baekjoon/Silver/S_2579/S3_2579_계단_오르기.java
@@ -0,0 +1,31 @@
+package Baekjoon.Silver.S_2579;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S3_2579_계단_오르기 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufRead = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufRead.readLine());
+        int[] stairs = new int[N+1];
+        int[] dp = new int[N+1];
+
+        for (int i = 1; i < N+1; i++) {
+            stairs[i] = Integer.parseInt(bufRead.readLine());
+        }
+
+        if (N == 1) { System.out.println(stairs[1]); return; }
+        if (N == 2) { System.out.println(stairs[1] + stairs[2]); return; }
+        
+        dp[1] = stairs[1];
+        dp[2] = stairs[1] + stairs[2];
+        dp[3] = Math.max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
+
+        for (int i = 4; i < N+1; i++) {
+            dp[i] = Math.max(dp[i-2] + stairs[i], dp[i-3] + stairs[i-1] + stairs[i]);
+        }
+        System.out.println(dp[N]);
+    }
+}
