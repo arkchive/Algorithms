ce7280ab42d7845aa0d1b498038facd1f40ca84c
arkchive <kyungminkim7@konkuk.ac.kr>
2025-10-20 13:11:55 +0900

    solve: 백준 silver3 [#11726] 2xn 타일링

---
+++ Baekjoon/Silver/S_11726/S3_11726_2xn_타일링.java
@@ -0,0 +1,26 @@
+package Baekjoon.Silver.S_11726;
+
+import java.util.Scanner;
+
+public class S3_11726_2xn_타일링 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        int[] dp = new int[n+1];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
