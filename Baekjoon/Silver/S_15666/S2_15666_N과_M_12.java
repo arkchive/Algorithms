c93528419fc03707bc2a9d3a627049fc02bb4968
arkchive <kyungminkim7@konkuk.ac.kr>
2025-10-21 15:47:28 +0900

    solve: 백준 silver2 [#15666] N과 M 12

---
+++ Baekjoon/Silver/S_15666/S2_15666_N과_M_12.java
@@ -0,0 +1,52 @@
+package Baekjoon.Silver.S_15666;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class S2_15666_N과_M_12 {
+    
+    static int N, M;
+    static int[] arr, nums;
+    static StringBuilder strBuild = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufRead = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer strToken = new StringTokenizer(bufRead.readLine());
+
+        N = Integer.parseInt(strToken.nextToken());
+        M = Integer.parseInt(strToken.nextToken());
+
+        arr = new int[M];
+        nums = new int[N];
+
+        strToken = new StringTokenizer(bufRead.readLine());
+        for (int i = 0 ; i < N; i++) {
+            nums[i] = Integer.parseInt(strToken.nextToken());
+        }
+
+        Arrays.sort(nums);
+        dfs(0, 0);
+        System.out.println(strBuild.toString());
+    }
+
+    static void dfs(int depth, int start) {
+        if (depth == M) {
+            for (int i : arr) {
+                strBuild.append(i).append(" ");
+            }
+            strBuild.append("\n");
+            return;
+        }
+
+        int prev = Integer.MIN_VALUE;
+        for (int i = start; i < N; i++) {
+            if (nums[i] == prev) continue;
+            arr[depth] = nums[i];
+            prev = nums[i];
+            dfs(depth + 1, i);
+        }
+    }
+}
