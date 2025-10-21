44180e51c9e294d91eea19778ba31d4ef7af5ca0
arkchive <kyungminkim7@konkuk.ac.kr>
2025-10-21 15:46:31 +0900

    solve: 백준 silver3 [#15657] N과 M 8

---
+++ Baekjoon/Silver/S_15657/S3_15657_N과_M_8.java
@@ -0,0 +1,49 @@
+package Baekjoon.Silver.S_15657;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class S3_15657_N과_M_8 {
+
+    static int N, M;
+    static int[] arr, nums;
+    static StringBuilder strBuild = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufRead = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer strToken = new StringTokenizer(bufRead.readLine());
+
+        N = Integer.parseInt(strToken.nextToken());
+        M = Integer.parseInt(strToken.nextToken());
+
+        arr = new int[M];
+        nums = new int[N];
+
+        strToken = new StringTokenizer(bufRead.readLine());
+        for (int i = 0 ; i < N; i++) {
+            nums[i] = Integer.parseInt(strToken.nextToken());
+        }
+
+        Arrays.sort(nums);
+        dfs(0, 0);
+        System.out.println(strBuild.toString());
+    }
+
+    static void dfs(int depth, int start) {
+        if (depth == M) {
+            for (int i : arr) {
+                strBuild.append(i).append(" ");
+            }
+            strBuild.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            arr[depth] = nums[i];
+            dfs(depth + 1, i);
+        }
+    }
+}
